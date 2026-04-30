b5d7a2f7bd1563e9e54093bbaa5f2704dbcd5c67
sagona <sagonafashion@gmail.com>
2026-04-30 22:31:43 +0100

    final production update

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -29,7 +29,7 @@ export const protect = async (req, res, next) => {
 /* =========================
    ADMIN ONLY
 ========================= */
-export const adminOnly = (req, res, next) => {
+export const admin = (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     next();
   } else {
